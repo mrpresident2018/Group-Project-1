b511dac777719d577dff6bd0b99349d26f9c7f30
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-10-10 09:24:08 -0400

    Hi

--- Project 1.1/src/SaurabhBlib.java
+++ Project 1.1/src/SaurabhBlib.java
@@ -1,17 +1,25 @@
 
 public class SaurabhBlib 
 {
-	public static void isPalindrome() 
+	public static void datestr() 
 	{
 		
 	}
-	
 	public static void sumupto(int x) 
 	{
 		int y = 0;
 		for (int i=0; i <= 0; i++)
 		{
 			y = i + x;
+		}
+		System.out.print (y);
+	}
+	public static void isfactorial(int x)
+	{
+		int y = 0;
+		for (int i=0; i <= 0; i++)
+		{
+			y = i*y;
 		}
 		System.out.print (y);
 	}
