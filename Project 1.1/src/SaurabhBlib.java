c42ee0805aa99dacb58a0b820bb9419aa424fe21
mrpresident2018 <sbansal3625@bths.edu>
2017-10-10 20:29:17 -0400

    Hi

--- Project 1.1/src/SaurabhBlib.java
+++ Project 1.1/src/SaurabhBlib.java
@@ -3,7 +3,12 @@ public class SaurabhBlib
 {
 	public static void datestr() 
 	{
-		
+//Dates are in mm/dd/yyyy
+//Must make in to dd-mm-yyyy
+		String a = str.substring (4,5);
+		String b = str.substring (1,2);
+		String c = str.substring (10,9,8,7);
+			System.out.print ( a + "-" + b "-" + c + "-");
 	}
 	public static void sumupto(int x) 
 	{
