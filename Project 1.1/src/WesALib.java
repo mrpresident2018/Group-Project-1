e91c1865f79c67bb305403de6b2e44df12dd2d9e
Wes Avedisian <Wes Avedisian@Chili_Ranch>
2017-10-15 21:52:40 -0400

    ay

--- Project 1.1/src/WesALib.java
+++ Project 1.1/src/WesALib.java
@@ -5,8 +5,32 @@ public class WesALib
 	{
 		
 	}
-	public static void isFibonnaci()
+	public static void isFibonnaci(int f)
 	{
+		if(f<0)
+		{
+			System.out.print("Not a Fibonacci");
+		}
+		else
+		{
+			int x=0;
+			int y=1;
+			int z=0;
+			while(c<n)
+			{
+				x+y=z;
+				x=y;
+				y=z;
+			}
+			if(c==n)
+			{
+				System.out.print("Is a Fibonacci");
+			}
+			else
+			{
+				System.out.print("Not a Fibonacci");
+			}
+		}
 		
 	}
 	public static void multiplacationTable()
